51883cf03b928831427de1ddc215cb65315cd7fe
Nikola Kabasaj <nikolakabasaj6@gmail.com>
2020-04-03 09:42:43 +0200

    Added clock on home screen

--- Weather-forecast/Weather-forecast/MainWindow.xaml.cs
+++ Weather-forecast/Weather-forecast/MainWindow.xaml.cs
@@ -13,8 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using Weather_forecast.JSONMapper;
 using Weather_forecast.Models;
+using Weather_forecast.Utility;
 using Weather_forecast.Web_mapping;
 
 namespace Weather_forecast
@@ -25,12 +27,25 @@ namespace Weather_forecast
     public partial class MainWindow : Window
     {
         public static Forecast forecast = new Forecast();
+        public Clock clock;
 
         public MainWindow()
         {
             InitializeComponent();
+            clock = new Clock(clockTicker);
+            CurrentCity c = new CurrentCity();
+           
         }
 
+        public void clockTicker(object sender, EventArgs e)
+        {   
+            clock.tickIncrement++;
+            string[] values = clock.getCurrentTime();
+            string hoursMinutes = values[0] + ":" + values[1];
+
+            clockTime.Content = hoursMinutes;
+            clockSeconds.Content = values[2];
+        }
         private void tableView_Click(object sender, RoutedEventArgs e)
         {
 
@@ -47,7 +62,7 @@ namespace Weather_forecast
             forecast.storeLocationForecast(searchText.Text);
             LocationForecast lf = forecast.getLocationForecast(searchText.Text);
             string str = "05-04-2020";
-            DateTime date = DateTime.ParseExact(str, "dd-MM-yyyy", null);
+            DateTime date = DateTime.ParseExact(str, "dd-MM-yyyy ", null);
             double tmp = lf.ForecastDict[date].Temp;
             int a = 2;
         }
---
+++ Weather-forecast/Weather-forecast/Utility/Clock.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Weather_forecast.Utility
+{
+    public class Clock
+    {
+        public DispatcherTimer timer = new DispatcherTimer();
+        public int tickIncrement = 0;
+
+        public Clock(EventHandler clockTicker)
+        {
+            timer.Interval = TimeSpan.FromSeconds(0.5);
+            timer.Tick += clockTicker;
+            timer.Start();
+        }
+
+        public string[] getCurrentTime()
+        {
+            int hours = DateTime.Now.Hour;
+            int minutes = DateTime.Now.Minute;
+            int seconds = DateTime.Now.Second;
+
+            if (seconds == 60)
+            {
+                seconds = 0;
+                minutes++;
+            }
+            if (minutes == 60)
+            {
+                minutes = 0;
+                hours++;
+            }
+
+            return validateAll(seconds, minutes, hours);
+        }
+       
+        private string[] validateAll(int seconds, int minutes, int hours)
+        {
+            string[] values = { validate(hours), validate(minutes), validate(seconds) };
+            return values;
+        }
+        private string validate(int num)
+        {
+            if(num < 10)
+            {
+                return "0" + num;
+            }
+            return num.ToString(); 
+        }
+    }
+}
